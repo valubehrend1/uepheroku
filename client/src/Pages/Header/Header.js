b92a81cd0bb6b2b01b428e972e3e9818d9297a32
Your NameValeria <valu-sp@hotmail.com>
2021-04-25 22:29:57 +0200

    Empezando tamaños

--- client/src/Pages/Header/Header.js
+++ client/src/Pages/Header/Header.js
@@ -10,13 +10,13 @@ const Header = () => {
     const [t] = useTranslation('HeaderTranslation');
     return (
         <Card className='text-center' id='header'>
-            <Card.Body className='body'>
-                <Card.Text> {t('title.title')} </Card.Text>
-                <Card.Text className='bold-text'>{t('subtitle.subtitle')}</Card.Text>
-                <p className='save-button'>{t('parrafo.linea')}</p>
+            <Card.Body className='header-body'>
+                <Card.Text className= "title"> {t('title.title')} </Card.Text>
+                <Card.Text className='bold-type'>{t('subtitle.subtitle')}</Card.Text>
+                <p className='save-button bold-type'>{t('parrafo.linea')}</p>
             </Card.Body>
         </Card>
-    );
+    );  
 };
 
 
