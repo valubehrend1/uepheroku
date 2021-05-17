0b931885b7c535ddb0e6ef8a98866129f0bc45bc
Your NameValeria <valu-sp@hotmail.com>
2021-05-17 21:17:46 +0200

    changed logo

--- client/src/Components/Svg/Svg.js
+++ client/src/Components/Svg/Svg.js
@@ -6,66 +6,178 @@ function SvgComponent(props) {
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
       viewBox="0 0 1169 584.5"
-      width={130}
+      width={125}
       {...props}
     >
-      <defs>
-        <path
-          d="M297.47 175.49C422.09 94.9 612.81 16.53 746.32 18.1c180.25 2.12 315.58 69.68 405.99 202.69 14.91 21.93 15.45 50.61 1.37 73.09-30.67 48.99-61.92 90.07-93.75 123.24-39.21 40.87-80.11 62.95-122.71 89.3-90.48 55.99-234.23 71.72-288.44 73.55-65.89 2.22-199.82-7.35-352.81-49.61-85.26-23.56-178.18-57.35-278.77-101.37-12.59-5.52-15.15-22.26-4.78-31.28 113.25-98.48 208.26-172.55 285.05-222.22z"
-          id="prefix__f2n2GQgBn"
-          style={{
-            animation: "cpVwWkEO_draw 2800ms ease-in-out 0ms forwards",
-          }}
-          strokeDasharray="2694 2696"
-          strokeDashoffset={2695}
-        />
-        <path
-          d="M813.03 309.76c5.23 26.76 16.99 48.14 35.28 64.13 27.44 23.99 45.21 28.43 69.93 27.63 58.4-1.91 98.84-48.38 98.84-99.97 0-51.59-35.89-97.27-94.64-103.11-26.63-2.65-60.52 11.85-79.97 32-12.97 13.43-22.78 33.65-29.44 60.65v236.13l-35 9.62"
-          id="prefix__d1UUQganhi"
-          style={{
-            animation: "cpVwWkEO_draw 2800ms ease-in-out 40ms forwards",
-          }}
-          strokeDasharray="892 894"
-          strokeDashoffset={893}
-        />
-        <path
-          d="M590.8 299.97h128.5c4.03 0 7.02-3.73 6.15-7.67-5.5-24.73-15.86-44.7-31.08-59.89-25.5-25.46-55.11-33.97-69.22-34.27-34.14-.72-104 23.66-106 103.91-.81 32.64 18.62 65.4 50.31 84.29 20.25 12.08 47.33 17.27 81.24 15.57"
-          id="prefix__c77H3M78oN"
-          style={{
-            animation: "cpVwWkEO_draw 2800ms ease-in-out 80ms forwards",
-          }}
-          strokeDasharray="639 641"
-          strokeDashoffset={640}
-        />
-        <path
-          d="M427.57 169.36V313.7c-14.64 59.27-48 88.67-100.1 88.2-52.09-.47-85.69-29.87-100.8-88.2v-56.43l43.4-30.13"
-          id="prefix__cJ1XIFtte"
-          style={{
-            animation: "cpVwWkEO_draw 2800ms ease-in-out 120ms forwards",
-          }}
-          strokeDasharray="546 548"
-          strokeDashoffset={547}
-        />
-        <path
-          d="M718.08 18.18C585.9 27.48 430.09 88.3 271.9 192.38c-1.21.79-7.24 4.75-18.09 11.88.17 32.55.26 50.63.28 54.25.28 59.84.07 57.11 4.91 69.9 6.38 16.99 21.46 32.93 38 40.27 11.57 5.1 25.67 7.34 37.52 5.94 12.69-1.47 22.44-4.9 32.47-11.4 15.35-9.99 26.29-24.95 31.13-42.85.13-.46.76-2.77 1.89-6.92l.42-61.16.35-61.17 2.25-4.68c2.8-5.94 7.43-10.56 13.39-13.35 4.14-1.96 5.4-2.24 10.66-2.17 8.48.07 12.97 1.89 19 7.9 3.37 3.36 5.05 5.73 6.31 8.81.12.28.68 1.68 1.69 4.19.12 33.56.19 52.2.21 55.92.14 36.07-.07 58.93-.56 64.31-1.55 17.41-5.47 31.18-13.33 46.84-16.55 32.92-45.93 56.55-81.69 65.71-13.04 3.35-20.54 4.12-35.97 3.63-14.87-.42-22.02-1.67-35.48-6.15-35.97-11.88-64.51-39-78.47-74.38-3.15-8.17-5.05-14.88-6.87-25.16-1.4-7.62-1.54-11.6-1.96-44.32-.03-2.39-.17-14.37-.42-35.93-7.79 5.79-12.11 9.01-12.98 9.65-55.6 41.45-121.1 95.97-153.42 127.85-17.53 17.2-22.44 24.89-21.6 33.83.63 7.55 3.23 11.12 12.2 16.99 19.92 13 113.53 49.91 186.66 73.54 188.35 60.95 358.96 84.51 504.18 69.62 17.6-1.75 44.88-5.66 59.6-8.53.77-.15 4.63-.89 11.57-2.23.21-82.21.33-127.88.35-137.01.42-149.18.21-140.44 4.28-156.94 12.13-49.07 53.99-87.16 104.83-95.48 10.59-1.75 29.45-1.75 39.9 0 41.93 6.92 77.06 32.92 95.72 70.67 17.67 35.86 17.46 77.45-.5 113.17-19.35 38.52-56.86 65.08-99.29 70.32-10.8 1.4-31.06.56-41.58-1.67-15.01-3.15-29.24-8.88-42.07-16.92-3.37-2.1-6.17-3.78-6.31-3.78-.14 0-.28 32.23-.28 71.66 0 45.57.28 71.65.7 71.65 1.26 0 23.49-7.55 35.06-11.89 44.95-16.98 85.97-39.28 123.76-67.38 63.25-47.05 114.51-109.75 153.78-188.25 8.9-17.76 10.8-25.17 9.32-37.12-1.82-14.54-5.46-20.55-31.2-50.47-71.8-83.6-156.58-138.34-251.81-162.52-28.04-7.13-55.53-11.4-87.3-13.64-9.04-.62-54.2-.76-61.35-.28-1.55.14-7.72.63-13.68 1.05zM700.2 199.86c20.48 16.22 35.83 38.73 43.41 63.75 3.64 12.1 4.91 20.34 4.91 32.72.07 10.34 0 11.04-1.83 14.82-3.85 8.17-11.71 14.26-20.54 15.86-5.47 1.05-105.18 1.05-110.51.07-8.35-1.6-15.64-7.06-19.78-14.96-2.1-3.98-2.24-4.68-2.24-11.6s.14-7.69 2.24-11.95c2.74-5.59 7.79-10.49 13.4-13.07.26-.13 1.59-.76 3.99-1.89l38.08-.35 38.07-.35c-1.72-3.48-2.68-5.42-2.87-5.8-9.68-19.16-27.56-33.42-48.88-38.73-9.11-2.31-26.01-2.24-34.85.07-26.85 6.99-46.91 26.49-54.13 52.64-2.24 8.11-3.02 21.32-1.68 29.99 4.27 29.15 26.08 53.47 54.62 60.81 2.95.77 10.1 1.82 15.92 2.31 9.26.84 11.01 1.19 14.72 3.07 4.98 2.52 11.01 8.6 12.91 13.08 3.01 7.06 3.01 15.37 0 22.36-1.9 4.48-8.28 10.91-13.12 13.29-3.64 1.81-4.97 2.09-12.27 2.3-25.59.84-54.55-8.87-77.34-25.93-7.85-5.94-20.83-19.22-26.44-27.05-15.42-21.74-23.28-44.6-24.19-70.95-.77-22.16 3.58-41.46 13.82-61.94 23.56-46.83 75.73-75.42 127.48-69.76 24.82 2.65 47.96 12.09 67.1 27.19z"
-          id="prefix__a1FewyeQ0l"
-          style={{
-            animation: "cpVwWkEO_draw 2800ms ease-in-out 160ms forwards",
-          }}
-          strokeDasharray="6040 6042"
-          strokeDashoffset={6041}
-        />
-        <path
-          d="M854.45 258.55c-21.39 32.09-13.75 76.27 17.18 99.48 24.19 18.17 56.87 20.06 82.6 4.68 17.39-10.35 29.17-26.14 34.43-46.28 1.4-5.38 1.61-7.9 1.61-17.47-.07-9.72-.28-12.1-1.89-17.97-8.91-33.34-38.5-56.06-72.65-55.71-3.71 0-9.18.49-12.2 1.05-19.91 3.56-37.86 15.38-49.08 32.22z"
-          id="prefix__c5ptOO6oKB"
-          style={{
-            animation: "cpVwWkEO_draw 2800ms ease-in-out 200ms forwards",
-          }}
-          strokeDasharray="466 468"
-          strokeDashoffset={467}
-        />
-      </defs>
-      <use xlinkHref="#prefix__f2n2GQgBn" />
+      <path
+        d="M297.47 175.49C422.09 94.9 612.81 16.53 746.32 18.1c180.25 2.12 315.58 69.68 405.99 202.69 14.91 21.93 15.45 50.61 1.37 73.09-30.67 48.99-61.92 90.07-93.75 123.24-39.21 40.87-80.11 62.95-122.71 89.3-90.48 55.99-234.23 71.72-288.44 73.55-65.89 2.22-199.82-7.35-352.81-49.61-85.26-23.56-178.18-57.35-278.77-101.37-12.59-5.52-15.15-22.26-4.78-31.28 113.25-98.48 208.26-172.55 285.05-222.22z"
+        id="prefix__f2n2GQgBn"
+        style={{
+          WebkitAnimationName: "cpVwWkEO_draw",
+          WebkitAnimationDuration: "2800ms",
+          WebkitAnimationIterationCount: "0ms",
+          WebkitAnimationTimingFunction: "ease-in-out",
+          WebkitAnimationFillMode: "forwards",
+          MozAnimationName: "cpVwWkEO_draw",
+          MozAnimationDuration: "2800ms",
+          MozAnimationIterationCount: "0ms",
+          MozAnimationTimingFunction: "ease-in-out",
+          MozAnimationFillMode: "forwards",
+          OAnimationName: "cpVwWkEO_draw",
+          OAnimationDuration: "2800ms",
+          OAnimationIterationCount: "0ms",
+          OAnimationTimingFunction: "ease-in-out",
+          OAnimationFillMode: "forwards",
+          animationName: "cpVwWkEO_draw",
+          animationDuration: "2800ms",
+          animationIterationCount: "0ms",
+          animationTimingFunction: "ease-in-out",
+          animationFillMode: "forwards",
+        }}
+        strokeDasharray="2694 2696"
+        strokeDashoffset={2695}
+      />
+      <path
+        d="M813.03 309.76c5.23 26.76 16.99 48.14 35.28 64.13 27.44 23.99 45.21 28.43 69.93 27.63 58.4-1.91 98.84-48.38 98.84-99.97 0-51.59-35.89-97.27-94.64-103.11-26.63-2.65-60.52 11.85-79.97 32-12.97 13.43-22.78 33.65-29.44 60.65v236.13l-35 9.62"
+        id="prefix__d1UUQganhi"
+        style={{
+          WebkitAnimationName: "cpVwWkEO_draw",
+          WebkitAnimationDuration: "2800ms",
+          WebkitAnimationIterationCount: "40ms",
+          WebkitAnimationTimingFunction: "ease-in-out",
+          WebkitAnimationFillMode: "forwards",
+          MozAnimationName: "cpVwWkEO_draw",
+          MozAnimationDuration: "2800ms",
+          MozAnimationIterationCount: "40ms",
+          MozAnimationTimingFunction: "ease-in-out",
+          MozAnimationFillMode: "forwards",
+          OAnimationName: "cpVwWkEO_draw",
+          OAnimationDuration: "2800ms",
+          OAnimationIterationCount: "40ms",
+          OAnimationTimingFunction: "ease-in-out",
+          OAnimationFillMode: "forwards",
+          animationName: "cpVwWkEO_draw",
+          animationDuration: "2800ms",
+          animationIterationCount: "40ms",
+          animationTimingFunction: "ease-in-out",
+          animationFillMode: "forwards",
+        }}
+        strokeDasharray="892 894"
+        strokeDashoffset={893}
+      />
+      <path
+        d="M590.8 299.97h128.5c4.03 0 7.02-3.73 6.15-7.67-5.5-24.73-15.86-44.7-31.08-59.89-25.5-25.46-55.11-33.97-69.22-34.27-34.14-.72-104 23.66-106 103.91-.81 32.64 18.62 65.4 50.31 84.29 20.25 12.08 47.33 17.27 81.24 15.57"
+        id="prefix__c77H3M78oN"
+        style={{
+          WebkitAnimationName: "cpVwWkEO_draw",
+          WebkitAnimationDuration: "2800ms",
+          WebkitAnimationIterationCount: "80ms",
+          WebkitAnimationTimingFunction: "ease-in-out",
+          WebkitAnimationFillMode: "forwards",
+          MozAnimationName: "cpVwWkEO_draw",
+          MozAnimationDuration: "2800ms",
+          MozAnimationIterationCount: "80ms",
+          MozAnimationTimingFunction: "ease-in-out",
+          MozAnimationFillMode: "forwards",
+          OAnimationName: "cpVwWkEO_draw",
+          OAnimationDuration: "2800ms",
+          OAnimationIterationCount: "80ms",
+          OAnimationTimingFunction: "ease-in-out",
+          OAnimationFillMode: "forwards",
+          animationName: "cpVwWkEO_draw",
+          animationDuration: "2800ms",
+          animationIterationCount: "80ms",
+          animationTimingFunction: "ease-in-out",
+          animationFillMode: "forwards",
+        }}
+        strokeDasharray="639 641"
+        strokeDashoffset={640}
+      />
+      <path
+        d="M427.57 169.36V313.7c-14.64 59.27-48 88.67-100.1 88.2-52.09-.47-85.69-29.87-100.8-88.2v-56.43l43.4-30.13"
+        id="prefix__cJ1XIFtte"
+        style={{
+          WebkitAnimationName: "cpVwWkEO_draw",
+          WebkitAnimationDuration: "2800ms",
+          WebkitAnimationIterationCount: "120ms",
+          WebkitAnimationTimingFunction: "ease-in-out",
+          WebkitAnimationFillMode: "forwards",
+          MozAnimationName: "cpVwWkEO_draw",
+          MozAnimationDuration: "2800ms",
+          MozAnimationIterationCount: "120ms",
+          MozAnimationTimingFunction: "ease-in-out",
+          MozAnimationFillMode: "forwards",
+          OAnimationName: "cpVwWkEO_draw",
+          OAnimationDuration: "2800ms",
+          OAnimationIterationCount: "120ms",
+          OAnimationTimingFunction: "ease-in-out",
+          OAnimationFillMode: "forwards",
+          animationName: "cpVwWkEO_draw",
+          animationDuration: "2800ms",
+          animationIterationCount: "120ms",
+          animationTimingFunction: "ease-in-out",
+          animationFillMode: "forwards",
+        }}
+        strokeDasharray="546 548"
+        strokeDashoffset={547}
+      />
+      <path
+        d="M718.08 18.18C585.9 27.48 430.09 88.3 271.9 192.38c-1.21.79-7.24 4.75-18.09 11.88.17 32.55.26 50.63.28 54.25.28 59.84.07 57.11 4.91 69.9 6.38 16.99 21.46 32.93 38 40.27 11.57 5.1 25.67 7.34 37.52 5.94 12.69-1.47 22.44-4.9 32.47-11.4 15.35-9.99 26.29-24.95 31.13-42.85.13-.46.76-2.77 1.89-6.92l.42-61.16.35-61.17 2.25-4.68c2.8-5.94 7.43-10.56 13.39-13.35 4.14-1.96 5.4-2.24 10.66-2.17 8.48.07 12.97 1.89 19 7.9 3.37 3.36 5.05 5.73 6.31 8.81.12.28.68 1.68 1.69 4.19.12 33.56.19 52.2.21 55.92.14 36.07-.07 58.93-.56 64.31-1.55 17.41-5.47 31.18-13.33 46.84-16.55 32.92-45.93 56.55-81.69 65.71-13.04 3.35-20.54 4.12-35.97 3.63-14.87-.42-22.02-1.67-35.48-6.15-35.97-11.88-64.51-39-78.47-74.38-3.15-8.17-5.05-14.88-6.87-25.16-1.4-7.62-1.54-11.6-1.96-44.32-.03-2.39-.17-14.37-.42-35.93-7.79 5.79-12.11 9.01-12.98 9.65-55.6 41.45-121.1 95.97-153.42 127.85-17.53 17.2-22.44 24.89-21.6 33.83.63 7.55 3.23 11.12 12.2 16.99 19.92 13 113.53 49.91 186.66 73.54 188.35 60.95 358.96 84.51 504.18 69.62 17.6-1.75 44.88-5.66 59.6-8.53.77-.15 4.63-.89 11.57-2.23.21-82.21.33-127.88.35-137.01.42-149.18.21-140.44 4.28-156.94 12.13-49.07 53.99-87.16 104.83-95.48 10.59-1.75 29.45-1.75 39.9 0 41.93 6.92 77.06 32.92 95.72 70.67 17.67 35.86 17.46 77.45-.5 113.17-19.35 38.52-56.86 65.08-99.29 70.32-10.8 1.4-31.06.56-41.58-1.67-15.01-3.15-29.24-8.88-42.07-16.92-3.37-2.1-6.17-3.78-6.31-3.78-.14 0-.28 32.23-.28 71.66 0 45.57.28 71.65.7 71.65 1.26 0 23.49-7.55 35.06-11.89 44.95-16.98 85.97-39.28 123.76-67.38 63.25-47.05 114.51-109.75 153.78-188.25 8.9-17.76 10.8-25.17 9.32-37.12-1.82-14.54-5.46-20.55-31.2-50.47-71.8-83.6-156.58-138.34-251.81-162.52-28.04-7.13-55.53-11.4-87.3-13.64-9.04-.62-54.2-.76-61.35-.28-1.55.14-7.72.63-13.68 1.05zM700.2 199.86c20.48 16.22 35.83 38.73 43.41 63.75 3.64 12.1 4.91 20.34 4.91 32.72.07 10.34 0 11.04-1.83 14.82-3.85 8.17-11.71 14.26-20.54 15.86-5.47 1.05-105.18 1.05-110.51.07-8.35-1.6-15.64-7.06-19.78-14.96-2.1-3.98-2.24-4.68-2.24-11.6s.14-7.69 2.24-11.95c2.74-5.59 7.79-10.49 13.4-13.07.26-.13 1.59-.76 3.99-1.89l38.08-.35 38.07-.35c-1.72-3.48-2.68-5.42-2.87-5.8-9.68-19.16-27.56-33.42-48.88-38.73-9.11-2.31-26.01-2.24-34.85.07-26.85 6.99-46.91 26.49-54.13 52.64-2.24 8.11-3.02 21.32-1.68 29.99 4.27 29.15 26.08 53.47 54.62 60.81 2.95.77 10.1 1.82 15.92 2.31 9.26.84 11.01 1.19 14.72 3.07 4.98 2.52 11.01 8.6 12.91 13.08 3.01 7.06 3.01 15.37 0 22.36-1.9 4.48-8.28 10.91-13.12 13.29-3.64 1.81-4.97 2.09-12.27 2.3-25.59.84-54.55-8.87-77.34-25.93-7.85-5.94-20.83-19.22-26.44-27.05-15.42-21.74-23.28-44.6-24.19-70.95-.77-22.16 3.58-41.46 13.82-61.94 23.56-46.83 75.73-75.42 127.48-69.76 24.82 2.65 47.96 12.09 67.1 27.19z"
+        id="prefix__a1FewyeQ0l"
+        style={{
+          WebkitAnimationName: "cpVwWkEO_draw",
+          WebkitAnimationDuration: "2800ms",
+          WebkitAnimationIterationCount: "160ms",
+          WebkitAnimationTimingFunction: "ease-in-out",
+          WebkitAnimationFillMode: "forwards",
+          MozAnimationName: "cpVwWkEO_draw",
+          MozAnimationDuration: "2800ms",
+          MozAnimationIterationCount: "160ms",
+          MozAnimationTimingFunction: "ease-in-out",
+          MozAnimationFillMode: "forwards",
+          OAnimationName: "cpVwWkEO_draw",
+          OAnimationDuration: "2800ms",
+          OAnimationIterationCount: "160ms",
+          OAnimationTimingFunction: "ease-in-out",
+          OAnimationFillMode: "forwards",
+          animationName: "cpVwWkEO_draw",
+          animationDuration: "2800ms",
+          animationIterationCount: "160ms",
+          animationTimingFunction: "ease-in-out",
+          animationFillMode: "forwards",
+        }}
+        strokeDasharray="6040 6042"
+        strokeDashoffset={6041}
+      />
+      <path
+        d="M854.45 258.55c-21.39 32.09-13.75 76.27 17.18 99.48 24.19 18.17 56.87 20.06 82.6 4.68 17.39-10.35 29.17-26.14 34.43-46.28 1.4-5.38 1.61-7.9 1.61-17.47-.07-9.72-.28-12.1-1.89-17.97-8.91-33.34-38.5-56.06-72.65-55.71-3.71 0-9.18.49-12.2 1.05-19.91 3.56-37.86 15.38-49.08 32.22z"
+        id="prefix__c5ptOO6oKB"
+        style={{
+          WebkitAnimationName: "cpVwWkEO_draw",
+          WebkitAnimationDuration: "2800ms",
+          WebkitAnimationIterationCount: "200ms",
+          WebkitAnimationTimingFunction: "ease-in-out",
+          WebkitAnimationFillMode: "forwards",
+          MozAnimationName: "cpVwWkEO_draw",
+          MozAnimationDuration: "2800ms",
+          MozAnimationIterationCount: "200ms",
+          MozAnimationTimingFunction: "ease-in-out",
+          MozAnimationFillMode: "forwards",
+          OAnimationName: "cpVwWkEO_draw",
+          OAnimationDuration: "2800ms",
+          OAnimationIterationCount: "200ms",
+          OAnimationTimingFunction: "ease-in-out",
+          OAnimationFillMode: "forwards",
+          animationName: "cpVwWkEO_draw",
+          animationDuration: "2800ms",
+          animationIterationCount: "200ms",
+          animationTimingFunction: "ease-in-out",
+          animationFillMode: "forwards",
+        }}
+        strokeDasharray="466 468"
+        strokeDashoffset={467}
+      />
+      <use xlinkHref="#prefix__f2n2GQgBn" fill="#2C272B" />
       <use
         xlinkHref="#prefix__d1UUQganhi"
         fillOpacity={0}
@@ -84,18 +196,18 @@ function SvgComponent(props) {
         stroke="#fff"
         strokeWidth={56}
       />
-      <use xlinkHref="#prefix__a1FewyeQ0l" />
-      <use xlinkHref="#prefix__c5ptOO6oKB" />
+      <use xlinkHref="#prefix__a1FewyeQ0l" fill="#2C272B" />
+      <use xlinkHref="#prefix__c5ptOO6oKB" fill="#2C272B" />
       <use
         xlinkHref="#prefix__c5ptOO6oKB"
         fillOpacity={0}
-        stroke="#000"
+        stroke="#2C272B"
         strokeWidth={8}
         strokeOpacity={0}
       />
-      <style data-made-with="vivus-instant">
+      <style>
         {
-          "@keyframes cpVwWkEO_draw{to{stroke-dashoffset:0}}@keyframes cpVwWkEO_fade{0%,94.44444444444444%{stroke-opacity:1}to{stroke-opacity:0}}"
+          "@-webkit-keyframes cpVwWkEO_draw{to{stroke-dashoffset:0}}@-moz-keyframes cpVwWkEO_draw{to{stroke-dashoffset:0}}@-o-keyframes cpVwWkEO_draw{to{stroke-dashoffset:0}}@keyframes cpVwWkEO_draw{to{stroke-dashoffset:0}}@-webkit-keyframes cpVwWkEO_fade{0%,94.44444444444444%{stroke-opacity:1}to{stroke-opacity:0}}@-moz-keyframes cpVwWkEO_fade{0%,94.44444444444444%{stroke-opacity:1}to{stroke-opacity:0}}@-o-keyframes cpVwWkEO_fade{0%,94.44444444444444%{stroke-opacity:1}to{stroke-opacity:0}}@keyframes cpVwWkEO_fade{0%,94.44444444444444%{stroke-opacity:1}to{stroke-opacity:0}}"
         }
       </style>
     </svg>
